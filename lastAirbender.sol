3be9267867cf01d6a310c142d9617fbf936c6267
Angshu Das <angshudas012@gmail.com>
2023-12-09 01:09:04 +0530

    Create lastAirbender.sol

---
+++ lastAirbender.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LastAirbender is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("lastAirbender", "Aang")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 12 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
